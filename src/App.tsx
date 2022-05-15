a2bbee178027b2743c7aa8e33ceb543ddddc28dd
Annie Sun <anniesun@Annies-MacBook-Pro.local>
2022-05-15 19:01:19 -0400

    bare bones

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {AppProvider, TextField, Button} from '@shopify/polaris';
+import enTranslations from '@shopify/polaris/locales/en.json';
+import axios from "axios";
+import style from './style.module.css';
 
+function API(prompt:string) {
+  const data = {
+    prompt: "Write a poem about a dog wearing skis",
+    max_tokens: 64,
+   };
+    
+   console.log(process.env.REACT_APP_SECRET_KEY)
+   axios.post("https://api.openai.com/v1/engines/text-curie-001/completions", data, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.REACT_APP_SECRET_KEY}`,
+    }
+   }).then((res) => {
+     console.log(res.data.choices);
+   });
+}
 function App() {
+  const [value, setValue] = useState("Jaded Pixel");
+
+  const handleChange = useCallback(
+    (newValue: string) => setValue(newValue),
+    []
+  );
   return (
+    <AppProvider i18n={enTranslations}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hi
+<div>
+<TextField
+        label="Store name"
+        value={value}
+        onChange={handleChange}
+        autoComplete="off"
+        multiline={8}
+        maxHeight={150}
+        inputMode="text"
+      />
+      <Button onClick={() => API('')}/>
+      
+</div>
     </div>
+    </AppProvider>
   );
 }
 
